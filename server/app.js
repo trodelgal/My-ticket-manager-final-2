7a4acb0e9a1ec3295098b2873ed375fee73c657b
trodelgal <67858041+trodelgal@users.noreply.github.com>
2020-08-24 13:51:20 +0300

    server GET entry point

--- server/app.js
+++ server/app.js
@@ -1,5 +1,22 @@
 const express = require('express');
-const app = express();
 
+const app = express();
+const fs = require('fs').promises;
+
+const dataPathFile = './data.json';
+app.use(express.json());
+
+app.get('/api/tickets', async (req, res) => {
+  const tickets = await fs.readFile(dataPathFile, 'utf-8');
+  const jsTickets = JSON.parse(tickets);
+  if (req.query.searchText) {
+    const filterTickets = jsTickets.filter(
+      (ticket) => ticket.title.toLowerCase().includes(req.query.searchText.toLowerCase()),
+    );
+    res.send(filterTickets);
+  } else {
+    res.send(jsTickets);
+  }
+});
 
 module.exports = app;
