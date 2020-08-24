6c0c2dd6aff034d6c2bb1d634ecf5d82ee481167
trodelgal <67858041+trodelgal@users.noreply.github.com>
2020-08-24 14:16:36 +0300

    server POST
    with 2 post entries point.
    test 3 is not passing

--- server/app.js
+++ server/app.js
@@ -19,4 +19,30 @@ app.get('/api/tickets', async (req, res) => {
   }
 });
 
+app.post('/api/tickets/:ticketId/done', async (req, res) => {
+  const tickets = await fs.readFile(dataPathFile, 'utf-8');
+  const jsTickets = JSON.parse(tickets);
+  jsTickets.forEach((ticket, i) => {
+    if (ticket.id === req.params.ticketId) {
+      jsTickets[i].done = true;
+    }
+  });
+  await fs.writeFile(dataPathFile, `${JSON.stringify(jsTickets)}`);
+
+  res.send({ updated: true });
+});
+
+app.post('/api/tickets/:ticketId/undone', async (req, res) => {
+  const tickets = await fs.readFile(dataPathFile, 'utf-8');
+  const jsTickets = JSON.parse(tickets);
+  jsTickets.forEach((ticket, i) => {
+    if (ticket.id === req.params.ticketId) {
+      jsTickets[i].done = false;
+    }
+  });
+  await fs.writeFile(dataPathFile, `${JSON.stringify(jsTickets)}`);
+
+  res.send({ updated: true });
+});
+
 module.exports = app;
